bc90bb762e0ba256678480f0a76045bc7fbb8143
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2021-01-07 17:58:52 -0300

    finished home component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <main></main>
+  <main>
+    <div class="text-top">Projeto para procurar vídeos</div>
+
+    <a href="https://github.com/DanielFreitasFerreira" target="_blank">
+      <img src="../assets/images/gitHub.png" alt="icon github">
+    </a>
+
+    <div class="text-bottom">github.com/DanielFreitasFerreira</div>
+  </main>
 </template>
 
 <script>
@@ -9,5 +17,29 @@ export default {
 </script>
 
 <style scoped>
+  main {
+    background: var(--color-background-home);
+    justify-content: center;
+    align-items: center;
+  }
+  img {
+    width: 12.75rem;
+  }
 
+  .text-top {
+    color: var(--color-text-light);
+    font-size: 1.37rem;
+    margin-bottom: 1.25rem;
+  }
+  .text-bottom {
+    color: var(--color-text-light);
+    font-size: 1.12rem;
+    margin-top: 1.25rem;
+  }
+
+  @media (min-width: 700px) {
+    img {
+      width: 15rem;
+    }
+  }
 </style>
