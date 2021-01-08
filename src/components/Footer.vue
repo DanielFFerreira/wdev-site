49b28fe3cf6aac2e94d69ba135f4c1ca2115a51b
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2021-01-07 23:55:04 -0300

    taking the data from the api and handling the data

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,26 @@
 <template>
-  <footer></footer>
+  <footer>
+    <a v-for="(socialLink,index) in socialLinks" :key="index" class="socialLink" :href="socialLink.link" target="_blank">
+      <img :src="socialLink.icon" :alt="socialLink.label" :title="socialLink.label">
+    </a>
+  </footer>
 </template>
 
 <script>
 import api from '@/services/api.js';
 
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  data() {
+    return {
+      socialLinks: []
+    }
+  },
+  mounted() {
+    api.get('/social-links.json').then(response => {
+      this.socialLinks = response.data;
+    });
+  }
 }
 </script>
 
@@ -18,5 +32,18 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+  .socialLink {
+    border: 2px solid var(--color-text-light);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    width: 2.5rem;
+    height: 2.5rem;
+    margin: 0 10px;
+  }
+  .socialLink img {
+    width: 1.25rem;
   }
 </style>
