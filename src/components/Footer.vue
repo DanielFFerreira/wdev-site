b273f711a6ac54fe6e3c713d1742e9eb1be2ae3c
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2021-01-07 21:52:35 -0300

    creating footer structure

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer></footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+  footer {
+    height: 60px;
+    width: 100%;
+    background: var(--color-background-nav);
+  }
+</style>
